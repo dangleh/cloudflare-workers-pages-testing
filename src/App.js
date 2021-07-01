c735f6f1e48340b7eb966bb0d866ebed0d010db5
LeHaiDang <lehaidangdev@gmail.com>
2021-07-01 09:53:43 +0700

    FIXED img tag

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import './App.css';
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const [query, setQuery] = useState("")
-  const [images, setImages] = useState([])
-  const updateQuery = evt => setQuery(evt.target.value)
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const updateQuery = (evt) => setQuery(evt.target.value);
 
   const fetchQuery = async () => {
-    await fetch("https://serverless-api.therealdang.workers.dev/", {
-      method: "POST",
+    await fetch('https://serverless-api.therealdang.workers.dev/', {
+      method: 'POST',
       body: JSON.stringify({
-        query
+        query,
       }),
       headers: {
         'Content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      }
-    }).then(response => response.json()).then(responseJson => setImages(responseJson));
-  }
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => setImages(responseJson));
+  };
 
   return (
     <div className="App">
       <div className="form">
-        <input id="query" type="text" onChange={updateQuery} placeholder="Search photos"/>
+        <input
+          id="query"
+          type="text"
+          onChange={updateQuery}
+          placeholder="Search photos"
+        />
         <button onClick={fetchQuery}>Search</button>
       </div>
-      {images.map(image => {
-        return <div>
-          <a key={image.id} href={image.link} target="_blank">
-          <image src={image.image}/>
-          </a>
-        </div>
+      {images.map((image) => {
+        return (
+          <div>
+            <a key={image.id} href={image.link} target="_blank">
+              <img src={image.image} />
+            </a>
+          </div>
+        );
       })}
     </div>
   );
